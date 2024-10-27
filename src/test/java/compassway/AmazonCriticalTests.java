1396891ef8449e3b91377c4a12a132090118652d
Igor Gudimov <igudimov@salucro.com>
2024-10-28 01:36:40 +0200

    Update gitignore file, add new libs to pom.xml, add files for AmazonCriticalTests and AmazonExtendedTests

---
+++ src/test/java/compassway/AmazonCriticalTests.java
@@ -0,0 +1,37 @@
+package compassway;
+
+import org.junit.jupiter.api.Test;
+
+public class AmazonCriticalTests {
+
+    @Test
+    public void verifySearch() {
+        // Verify search functionality on popular products (e.g., iPhone 12,13,14)
+
+    }
+
+    @Test
+    public void verifyProductFiltering() {
+        // Verify product filtering options work as expected (e.g., iPhone 14)
+
+    }
+
+    @Test
+    public void verifyDeliveryLocationChange() {
+        // Verify switching of delivery location and changing interface of main navigation bar
+
+    }
+
+    @Test
+    public void verifyShoppingCartAndCheckout() {
+        // Verify adding products to the shopping cart and checkout redirect to signin page (e.g., iPhone 14)
+
+    }
+
+    @Test
+    public void verifyProductPageDetails() {
+        // Verify fields on any product page (e.g., iPhone 14)
+
+    }
+
+}
---
+++ src/test/java/compassway/AmazonExtendedTests.java
@@ -0,0 +1,40 @@
+package compassway;
+
+import org.junit.jupiter.api.Test;
+
+public class AmazonExtendedTests {
+
+    @Test
+    public void verifyYourAccount() {
+        // Verify main services on Your Account page
+
+    }
+
+    @Test
+    public void verifySearchSelect() {
+        // Verify all options for searchSelect field
+        // #searchDropdownBox
+
+    }
+
+    @Test
+    public void verifyLocalizationChange() {
+        // Verify changes in localization
+        // https://www.amazon.com/customer-preferences/edit
+
+    }
+
+    @Test
+    public void verifyCurrencyChange() {
+        // Verify changes in currency
+        // https://www.amazon.com/customer-preferences/edit
+
+    }
+
+    @Test
+    public void verifyChangeCountryRegionWebsite() {
+        // Verify selecting new website for another country
+        // https://www.amazon.com/customer-preferences/country
+
+    }
+}
